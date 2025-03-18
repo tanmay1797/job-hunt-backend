ee92a2f46c589920a54ec07baff8aa27fcad88d0
Tanmay <tanmaygautam1797@gmail.com>
2025-03-18 11:51:23 +0530

    Fix: CORS and PORT for Render deployment

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://job-hunt-backend-g8o1.onrender.com",
   credentials: true,
 };
 
